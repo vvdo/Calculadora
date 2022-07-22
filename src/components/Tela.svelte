f7f3e20c2bedd61cc9e1233853d97cc14f13cf2d
Vitor Oliveira <71193550+vvdo@users.noreply.github.com>
2022-07-22 13:06:50 -0300

    Update Tela.svelte
    add

--- src/components/Tela.svelte
+++ src/components/Tela.svelte
@@ -14,7 +14,7 @@
         justify-content: flex-end;
         align-items: center;
         padding: 0px 5px;
-        font-size: 1.7rem;
+        font-size: 3rem;
         background-color: aliceblue;
         overflow: hidden;
         margin: 2px 1px;
@@ -22,17 +22,17 @@
         border-top-left-radius: 5px;
     }
 
-    .s-10 {font-size: 1.5rem;}
-    .s-11 {font-size: 1.35rem;}
-    .s-12 {font-size: 1.25rem;}
-    .s-13 {font-size: 1.15rem;}
-    .s-14 {font-size: 1.07rem;}
-    .s-15 {font-size: 1.0rem;}
-    .s-16 {font-size: 0.93rem;}
-    .s-17 {font-size: 0.87rem;}
-    .s-18 {font-size: 0.83rem;}
-    .s-19 {font-size: 0.78rem;}
-    .s-20 {font-size: 0.74rem;}
+    .s-10 {font-size: 2.8rem;}
+    .s-11 {font-size: 2.5rem;}
+    .s-12 {font-size: 2.35rem;}
+    .s-13 {font-size: 2.25rem;}
+    .s-14 {font-size: 2.17rem;}
+    .s-15 {font-size: 2.07rem;}
+    .s-16 {font-size: 2.0rem;}
+    .s-17 {font-size: 1.80rem;}
+    .s-18 {font-size: 1.73rem;}
+    .s-19 {font-size: 1.68rem;}
+    .s-20 {font-size: 1.54rem;}
 
-    .pequena {font-size: 0.6rem;}
+    .pequena {font-size: 1.30rem;}
 </style>
